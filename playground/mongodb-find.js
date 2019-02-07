fe353985754715c9b27ed587c8f0f715add87c16
nilsonivano <nilson.ivano@gmail.com>
2019-02-07 15:42:00 -0200

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,18 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+const obj = new ObjectID();
+console.log(obj);
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+  db.collection('Todos').find().toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos', err)
+  });
+  client.close();
+});
